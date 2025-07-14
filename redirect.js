243925ed1c2d3518ad9c5e118aaeef77718bf3a6
GLKESSE <kesselaurent1997@gmail.com>
2025-07-14 10:55:59 +0000

    ElanVie_Project

--- redirect.js
+++ redirect.js
@@ -1,15 +1,1 @@
-// Redirection automatique selon le type d'utilisateur
-const user = {
-  nom: "KOUASSI Adjoua",
-  type: "professionnel" // ou "patient"
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  if (user.type === "professionnel") {
-    window.location.href = "dashboard_pro.html";
-  } else if (user.type === "patient") {
-    window.location.href = "dashboard_patient.html";
-  } else {
-    alert("Type d'utilisateur inconnu.");
-  }
-});
+<!-- Script de redirection -->
